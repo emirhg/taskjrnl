7dbe799125be3e0c445920198df8087b98b66b48
Emir Herrera González <emirherrera@creai.mx>
2025-06-20 23:27:25 -0600

    fix: Use temp file instead of FIFO to avoid presenterm freeze

--- sh/presentlog.sh
+++ sh/presentlog.sh
@@ -2,8 +2,6 @@ function presentlog() {
     local script_dir=$(dirname $0:A)
     local fifo_path="/tmp/presentlog.fifo"
 
-    mkfifo $fifo_path
-
     tlog $@ --export json | ${script_dir}/python/jrnl-to-presenterm.py --tags-position=start --time >$fifo_path &
 
     presenterm $fifo_path
