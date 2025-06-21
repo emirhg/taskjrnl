8c96f9f59569fbb4a643c19a0c29b34e87c66468
Emir Herrera González <emirherrera@creai.mx>
2025-06-21 01:15:47 -0600

    fix: Remove tags slide

--- sh/presentlog.sh
+++ sh/presentlog.sh
@@ -2,7 +2,7 @@ function presentlog() {
     local script_dir=$(dirname $0:A)
     local fifo_path="/tmp/presentlog.fifo"
 
-    tlog $@ --export json | ${script_dir}/python/jrnl-to-presenterm.py --tags-position=start --time >$fifo_path &
+    tlog $@ --export json | ${script_dir}/python/jrnl-to-presenterm.py --date >$fifo_path
 
     presenterm $fifo_path
     rm $fifo_path
